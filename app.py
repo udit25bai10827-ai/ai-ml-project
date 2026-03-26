d41161c6e6c536e17575999860487e0f5411f58a
Udit Sharma <udit.25bai10827@vitbhopal.ac.in>
2026-03-26 10:49:00 +0530

    app.py

--- app.py
+++ app.py
@@ -4,16 +4,32 @@ import pickle
 # Load model
 model = pickle.load(open("student_model.pkl", "rb"))
 
-st.title("🎓 Student Pass/Fail Predictor")
-
-st.write("Enter student details:")
+st.title("🎓 AI Student Performance Analyzer")
+st.write("Predict student performance using Machine Learning")
 
 # Inputs
 study_hours = st.number_input("Study Hours", 0, 12)
 attendance = st.number_input("Attendance (%)", 0, 100)
 previous_marks = st.number_input("Previous Marks", 0, 100)
 
+st.write("Example: Study Hours = 5, Attendance = 75, Marks = 55")
+
 # Prediction
 if st.button("Predict Result"):
+    
+    # Prediction
     prediction = model.predict([[study_hours, attendance, previous_marks]])
+    
+    # Confidence score
+    proba = model.predict_proba([[study_hours, attendance, previous_marks]])
+    confidence = max(proba[0]) * 100
+
+    # Output
     st.success(f"Result: {prediction[0]}")
+    st.write(f"Confidence: {confidence:.2f}%")
+
+    # Suggestion
+    if prediction[0] == "Fail":
+        st.warning("Suggestion: Increase study hours and improve attendance.")
+    else:
+        st.success("Good performance! Keep it up.")
